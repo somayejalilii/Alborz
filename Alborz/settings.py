184e71df94239490b61b462e3d4c9ccf69095912
PC <s68jalili@gmail.com>
2021-11-23 03:30:06 -0800

    2/9/1400

--- Alborz/settings.py
+++ Alborz/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'Alborz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Alborz',
-        'USER': 'postgres',
-        'PASSWORD': 'somaye0010789758',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Alborz',
+#         'USER': 'postgres',
+#         'PASSWORD': 'somaye0010789758',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
--- accounts/models.py
+++ accounts/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 
 # Create your models here.
-class Register(models.Model):
+class PersonalInformation(models.Model):
     first_name = models.CharField(max_length=50, verbose_name='نام')
     last_name = models.CharField(max_length=50, verbose_name='نام خانوادگی')
-    avg = models.IntegerField(verbose_name='معدل')
-    grade = models.CharField(max_length=50, verbose_name='مقطع تحصیلی')
+    father_name = models.CharField(max_length=50, verbose_name='نام پدر')
+    phone_number = models.CharField(max_length=11, verbose_name='شماره تماس')
+    national_code = models.CharField(max_length=10, verbose_name='کد کلی')
+    email = models.EmailField(verbose_name='ایمیل')
+
+    def __str__(self):
+        return self.email
+
+
+class EducationalInformation(models.Model):
+    GRADE = [('bachelors', 'کارشناس'), ('masters', 'کارشناسی ارشد'), ('Doctorate', 'دکترا')]
+    ORIENTATION = [('مهندسی', 'مهندسی'), ('پزشکی', 'پزشکی'), ('علوم انسانی', 'علوم انسانی'),
+                   ('علوم حوزوی', 'عوم حوزوی')]
+    orientation = models.CharField(choices=ORIENTATION, max_length=20, verbose_name='گرایش ')
+    grade = models.CharField(choices=GRADE, max_length=50, verbose_name='مقطع تحصیلی')
     field_study = models.CharField(max_length=50, verbose_name='رشته تحصیلی')
+    avg = models.IntegerField(verbose_name='معدل')
     university = models.CharField(max_length=50, verbose_name='دانشگاه')
     education_place = models.CharField(max_length=50, verbose_name='محل تحصیل')
     degree_education = models.ImageField(upload_to='about/', null=True, blank=True, verbose_name='مدرک تحصیلی')
-
-    def get_full_name(self):
-        """
-        Return the first_name plus the last_name, with a space in between.
-        """
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    @property
-    def full_name(self):
-        return self.get_full_name()
-
-    def __str__(self):
-        return self.get_full_name()
